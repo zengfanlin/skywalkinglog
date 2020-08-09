b4deaa20b6a90370e8b6ed377daf30e354587967
fanlin.zeng <243238340@qq.com>
2020-08-09 23:43:36 +0800

    自定义async  appender Signed-off-by: fanlin.zeng <243238340@qq.com>

---
+++ consumer/src/main/java/com/example/consumer/SkLogAppender.java
@@ -0,0 +1,28 @@
+package com.example.consumer;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.UnsynchronizedAppenderBase;
+import org.apache.skywalking.apm.toolkit.trace.ActiveSpan;
+
+public class SkLogAppender extends UnsynchronizedAppenderBase<ILoggingEvent> {
+    @Override
+    protected void append(ILoggingEvent eventObject) {
+//        doc.setTime(DateFormatUtils.format(eventObject.getTimeStamp(), "yyyy-MM-dd HH:mm:ss"));
+//        doc.setLevel(eventObject.getLevel().toString());
+//        doc.setLogger(eventObject.getLoggerName());
+//        doc.setThread(eventObject.getThreadName());
+//        doc.setCurThread(Thread.currentThread().getName());
+//        doc.setMessage(eventObject.getFormattedMessage());
+
+        // 这里对log信息重新调用放到链路里
+        if (Level.ERROR.equals(eventObject.getLevel())) {
+            ActiveSpan.error(eventObject.getFormattedMessage());
+//            ActiveSpan.error(eventObject.getThrown());
+        } else if (Level.INFO.equals(eventObject.getLevel())) {
+            ActiveSpan.info(eventObject.getFormattedMessage());
+        } else if (Level.DEBUG.equals(eventObject.getLevel())) {
+            ActiveSpan.debug(eventObject.getFormattedMessage());
+        }
+    }
+}
